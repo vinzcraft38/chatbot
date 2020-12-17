a7ab7022a2ce505867fe64ac91bd537d04021bae
vinzcraft38 <vinzcraft@hotmail.com>
2020-12-17 10:46:05 +0100

    initial commit

---
+++ v1/chatbot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env phyton3
+# -*- coding: utf-8 -*-
+
+# Ein einfacher Chatbot
+# (c) 2020 by me, Lizence GLPv3
+
+print("Willkommen beim chatbot (v1)")
+print("Woruber wollensir sprechen?")
+print("Zum Beenden geben Sie bye ein...")
+print("")
+
+
+nutzereingabe = ""
+nutzereingabe = input("Ihre Frage oder Antwort: ")
+print(nutzereingabe)
