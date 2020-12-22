123787325880fa77a0b282f55d61571cb4bb1894
vinzcraft <vinzcraft@hotmail.com>
2020-12-22 12:28:10 +0100

    Web-GUI fertig

--- v2/chatbot-webgui.py
+++ v2/chatbot-webgui.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-
-from flask import Flask
-import flask
+from flask import Flask, render_template, request, flash
+from chatbot import Chatbot
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Pa$$w0rd'
 
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
+
+
 def index():
-    return "Hallo Welt!"
+
+    chatbot_label = "Hallo. Worüber wollen Sie sprechen?"
+
+    # Listen
+    zufallsantworten = ["Oh wirklich...", "Interessant", "Das kann man so sehen.", "Ich verstehe..."]
+    reaktionen = {"hallo": "aber hallo",
+                "geht": "Was verstehst Du darunter",
+                "schmeckt": "Ich habe keinen Geschmackssinn"}
+    
+    if request.method == 'POST':
+        chatbot_input = request.form['chatbot_input']
+        if not chatbot_input:
+            flash('Ohne Frage kann ich nicht antworten!')
+        else:
+            bot = Chatbot(reaktionen, zufallsantworten)
+            bot.set_Message(chatbot_input)
+            chatbot_label = bot.get_response()
+
+    return render_template('chatbot.html', chatbot_label=chatbot_label)
